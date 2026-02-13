b43ddea6318c6fa44c5edde7b611bac982990493
Ghomermesina1975 <>
2026-02-13 00:47:36 +0000

    Update and Delete

---
+++ ddl_and_dml_in_sql/update_and_delete/query.sql
@@ -0,0 +1,15 @@
+UPDATE medications
+SET price = 10
+WHERE price < 10;
+
+SELECT * 
+FROM medications
+ORDER BY id;- Write your solution here
+
+
+
+
+-- Please do not modify the code below!
+SELECT * FROM medications
+ORDER BY id;
+-- Please do not modify the code above!
